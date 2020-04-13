a8d921a229fafb8e29044ce9b96b809286c6fb70
vishal-nigade <vshlnigade71@gmail.com>
2020-04-13 11:42:15 +0530

    Added App led indication and Board stuck and reset facility

--- IOT_product.ino
+++ IOT_product.ino
@@ -1,66 +1,26 @@
-#define BLYNK_PRINT Serial // Defines the object that is used for printing
-//#define BLYNK_DEBUG        // Optional, this enables more detailed prints
-
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <EEPROM.h>
-//#include <SimpleTimer.h>
-
-#define EEPROM_WIFI_VALID   0x1
-#define SSID_LENGH(val)     ((val >> 1) 0x1F)
-
-SimpleTimer connectionHandlerTimer;
-struct board_info{
-  char auth[40];
-  char ssid[20];
-  char pass[20];
-  uint32_t ssidlen;
-  uint32_t passlen;
-  uint8_t update_wifi;
-}g_bd;
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "rVfX8DIkZvMjNfoPgAIiTS0TEu7CvUXr";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "SAYALI";
-char pass[] = "sayali@123";
-
-//Pin Declaration
-const int LED = D0;
-
-typedef enum {
-  CONNECT_TO_WIFI,
-  AWAIT_WIFI_CONNECTION,
-  CONNECT_TO_BLYNK,
-  AWAIT_BLYNK_CONNECTION,
-  MAINTAIN_CONNECTIONS,
-  AWAIT_DISCONNECT
-} CONNECTION_STATE;
-
-CONNECTION_STATE connectionState;
-uint8_t connectionCounter;
+#include "defs.h"
 
 void ConnectionHandler(void) {
   switch (connectionState) {
   case CONNECT_TO_WIFI:
-    BLYNK_LOG("Connecting to %s.", ssid);
-    WiFi.begin(ssid, pass);
+    BLYNK_LOG("Connecting to %s.", g_bd.ssid);
+    WiFi.begin(g_bd.ssid, g_bd.pass);
     connectionState = AWAIT_WIFI_CONNECTION;
     connectionCounter = 0;
     break;
 
   case AWAIT_WIFI_CONNECTION:
     if (WiFi.status() == WL_CONNECTED) {
-      BLYNK_LOG("Connected to %s", ssid);
+      BLYNK_LOG("Connected to %s", g_bd.ssid);
       connectionState = CONNECT_TO_BLYNK;
     }
     else if (++connectionCounter == 50) {
-      BLYNK_LOG("Unable to connect to %s. Retry connection.", ssid);
+      wifi.off();
+      BLYNK_LOG("Unable to connect to %s. Retry connection.", g_bd.ssid);
       WiFi.disconnect();
       connectionState = AWAIT_DISCONNECT;
       connectionCounter = 0;
+      
     }
     break;
 
@@ -74,6 +34,9 @@ void ConnectionHandler(void) {
 
   case AWAIT_BLYNK_CONNECTION:
     if (Blynk.connected()) {
+      reboot_counter = 0;
+      wifi.on();
+      blynk.on();
       BLYNK_LOG("Connected to Blynk server.");
       connectionState = MAINTAIN_CONNECTIONS;
     }
@@ -88,6 +51,7 @@ void ConnectionHandler(void) {
 
   case MAINTAIN_CONNECTIONS:
     if (WiFi.status() != WL_CONNECTED) {
+      wifi.off();
       BLYNK_LOG("Wifi connection lost. Reconnect.");
       Blynk.disconnect();
       WiFi.disconnect();
@@ -95,39 +59,65 @@ void ConnectionHandler(void) {
       connectionCounter = 0;
     }
     else  if (!Blynk.connected()) {
+      blynk.off();
       BLYNK_LOG("Blynk server connection lost. Reconnect.");
       Blynk.disconnect();
       connectionState = CONNECT_TO_BLYNK;
     }
     else {
       Blynk.run();
+      UpdateEEPROM();
     }
     break;
 
   case AWAIT_DISCONNECT:
     if (++connectionCounter == 10) {
       connectionState = CONNECT_TO_WIFI;
+      ++reboot_counter;
     }
+    if(reboot_counter == 10){
+      DisEEPROMWifiFlg();
+      Serial.println("Rebooring ESP");
+      ESP.restart();
+      while(1);
+    }
+    Serial.println("count = ");
+    Serial.println(connectionCounter, DEC);
+    Serial.println(reboot_counter, DEC);
+    break;
+  case USER_WIFI_UPDATE_REQ:
+    Blynk.disconnect();
+    WiFi.disconnect();
+    connectionState = AWAIT_DISCONNECT;
+    connectionCounter = 0;
+    g_bd.update_wifi = 0;
     break;
   }
 }
+
+
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
   EEPROM.begin(512);
-  digitalWrite(LED, LOW);
+  digitalWrite(LED, HIGH);
 
-  uint8_t ssid_lengh;
-  uint16_t pre_prgm = GetEEPROMStatus();
+  uint8_t ssid_lengh, pass_lengh;
+  uint32_t pre_pgm;
+  pre_pgm = GetEEPROMStatus();
   if(pre_pgm & EEPROM_WIFI_VALID){
     ssid_lengh = SSID_LENGH(pre_pgm);
     pass_lengh = PASS_LENGH(pre_pgm);
+    Serial.print("SSID lengh = ");
+    Serial.println(ssid_lengh);
+    Serial.print("PASS lengh = ");
+    Serial.println(pass_lengh);
     GetEEPROMData(ssid_lengh, g_bd.ssid, SSID_EEPROM_START_ADDR);
-    GetEEPROMData(ssid_lengh, g_bd.ssid, PASS_EEPROM_START_ADDR);
+    GetEEPROMData(pass_lengh, g_bd.pass, PASS_EEPROM_START_ADDR);
   }
-    
+  
   connectionHandlerTimer.setInterval(100, ConnectionHandler);
   connectionState = CONNECT_TO_WIFI;
 }
